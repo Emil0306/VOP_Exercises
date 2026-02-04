caf774db89fb7f59e10d4bf8439ab8bc2742d8a3
Emil Nielsen <emil0306dk@gmail.com>
2026-02-04 16:00:42 +0100

    Added design patterns exercises

---
+++ design-patterns/CircularArray/CaesarCipher.cs
@@ -0,0 +1,18 @@
+﻿namespace CircularArray;
+
+public class CaesarCipher
+{
+    private char[] _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+    public string Encode(string text, int shift)
+    {
+        // TODO: Implement encoding logic here
+        throw new NotImplementedException(); 
+    }
+
+    public string Decode(string text, int shift)
+    {
+        // TODO: Implement encoding logic here
+        throw new NotImplementedException();  
+    }
+}
---
+++ design-patterns/Singleton/MyClass.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns;
+
+public class MyClass
+
+{
+    private int _value;
+
+    public MyClass(int value)
+    {
+        _value = value;
+    }
+
+    public void PrintValue()
+    {
+        Console.WriteLine("Value= "+ _value);
+    }
+}
